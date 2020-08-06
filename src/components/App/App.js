44babefb4f72752bfb8065da77bc198fe403c54f
Stephanos Criticos <scritico@unb.ca>
2020-08-06 12:54:46 -0400

    exercise 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import GlobalStyles from "../GlobalStyles";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <Router>
+      <GlobalStyles />
+      TODO
+    </Router>
+  );
 };
 
 export default App;
